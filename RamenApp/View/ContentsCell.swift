5c78906dccfe6187a1f52e2dc7815c3401368713
Kai Aoyama <sauna.kaikai@gmail.com>
2021-05-27 21:38:48 +0900

    プロフィール画面-フォロー機能実装

---
+++ RamenApp/View/ContentsCell.swift
@@ -0,0 +1,23 @@
+//
+//  ContentsCell.swift
+//  RamenApp
+//
+//  Created by ヘパリン類似物質 on 2021/05/27.
+//
+
+import UIKit
+
+class ContentsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
