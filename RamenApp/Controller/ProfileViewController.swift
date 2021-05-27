d39156e5bfe8128137b3e640537274bb089df8d0
Kai Aoyama <sauna.kaikai@gmail.com>
2021-05-28 07:51:15 +0900

    フォローフォロワー数関係、作成

--- RamenApp/Controller/ProfileViewController.swift
+++ RamenApp/Controller/ProfileViewController.swift
@@ -6,24 +6,176 @@
 //
 
 import UIKit
+import FirebaseAuth
+import SDWebImage
+import Cosmos
+import SSSpinnerButton
 
-class ProfileViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+//Detailから遷移してきた場合(他人のプロフィール)と、自分のプロフィール画面、二つの画面で使用する。
+class ProfileViewController: UIViewController,UITableViewDelegate,UITableViewDataSource,GetDataProtocol,GetProfileDataProtocol,DoneFollowAction, GetFollowDataProtocol, GetFollowersDataProtocol {
+    
+    //モデルのインスタンス化
+    var loadModel = LoadModel()
+    var sendDBModel = SendDBModel()
+    
+    //投稿詳細画面から遷移してきた際に、遷移元の投稿の詳細が格納されるインスタンス
+    var contentModel:ContentModel?
+    
+    //loadModelから入手してきたデータを入れる配列
+    var contentModelArray = [ContentModel]()
+    var followerArray = [FollowerModel]()
+    var followArray = [FollowModel]()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var followLabel: UILabel!
+    @IBOutlet weak var followerLabel: UILabel!
+    @IBOutlet weak var followButton: SSSpinnerButton!
+    @IBOutlet weak var profileTextLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //プロトコル委任関係
+        tableView.delegate = self
+        tableView.dataSource = self
+        sendDBModel.doneFollowAction = self
+        
+        //カスタムセルが使用できるように設定
+        tableView.register(UINib(nibName: "ContentsCell", bundle: nil), forCellReuseIdentifier: "Cell")
+        //プロフィール画像のデザイン記述
+        imageView.layer.cornerRadius = imageView.frame.width/2
+        
+        //自分のプロフィールページかそうでないかで場合分け
+        if self.tabBarController?.selectedIndex == 2{
+            //もし、タブバーを使用して自分のプロフィールページを見ている場合の処理 => フォローボタンを消す
+            followButton.isHidden = true
+            //ログインしているユーザー（自分）の場合なので、現在の自分のIDでsetUpを行う
+            setUp(userID: Auth.auth().currentUser!.uid)
+            
+        } else {
+            
+            if contentModel?.userID == Auth.auth().currentUser?.uid{
+                //自分の投稿から自分のプロフィールページに渡ってきた場合  => フォローボタンを消す
+                followButton.isHidden = true
+            }
+            
+            //自分のプロフィール画面でない時は、遷移前画面から受け取ったcontentModelの情報を使ってsetUpを行う.
+            setUp(userID: (contentModel?.userID)!)
+            //プロフィール情報を画面に反映
+            imageView.sd_setImage(with: URL(string: (contentModel?.sender![0])!), completed: nil)
+            imageView.layer.cornerRadius = 20
+            imageView.clipsToBounds = true
+            userNameLabel.text = contentModel?.sender![3]
+            profileTextLabel.text = contentModel?.sender![1]
+            
+        }
     }
-    */
-
+    
+    //userIDを引数にとって、ユーザの情報を取得するメソッドを作成
+    func setUp(userID:String){
+        
+        loadModel.getDataProtocol = self
+        loadModel.getProfileDataProtocol = self
+        loadModel.getFollowDataProtocol = self
+        loadModel.getFollowersDataProtocol = self
+        //表示ユーザのプロフィールを取得
+        loadModel.loadProfile(userID: userID)
+        //フォロワーデータを取得
+        loadModel.getFollowerData(userID: userID)
+        //フォローデータを取得
+        loadModel.getFollowData(userID: userID)
+        //投稿コンテンツの取得
+        loadModel.loadOwnContents(userID: userID)
+        
+    }
+    
+    
+    @IBAction func follow(_ sender: Any) {
+        
+        //フォロー処理が終了するまで、アニメーションを行う。
+        
+        followButton.startAnimate(spinnerType: .ballClipRotate, spinnercolor: .blue, spinnerSize: 20) {
+            
+            //もしボタン表記が「フォローをする」だったら、followOrnot == trueにして、相手をフォローする。
+            if self.followButton.titleLabel?.text == "フォローをする" {
+                
+                self.sendDBModel.followAction(userID: (self.contentModel?.userID)!, followOrNot: true, contentModel: self.contentModel!)
+                
+                //もしボタン表記が「フォローをする」だったら、followOrnot == falseにして、相手のフォローを外す。
+            }else if self.followButton.titleLabel?.text == "フォローをやめる"{
+                self.sendDBModel.followAction(userID: (self.contentModel?.userID)!, followOrNot: false, contentModel: self.contentModel!)
+            }
+        }
+    }
+    
+    //フォローボタンをタップした後、条件によってフォローボタンの表記を変更するデリゲートメソッド(ボタンの表記を変える際、)
+    func checkFollow(flag: Bool) {
+        //もし、フォローをした後ならば!
+        if flag == true {
+            self.followButton.stopAnimatingWithCompletionType(completionType: .none) {
+                
+                self.followButton.setTitle("フォローをやめる", for: .normal)
+                
+            }
+            //もし、フォローを外した後ならば
+        }else{
+            self.followButton.stopAnimatingWithCompletionType(completionType: .none) {
+                
+                self.followButton.setTitle("フォローをする", for: .normal)
+            }
+        }
+    }
+    
+    
+    //フォローに関するデリゲートメソッド
+    func getFollowData(followArray: [FollowModel]) {
+        
+        self.followArray = []
+        self.followArray = followArray
+        //フォロー数を反映させる。
+        followLabel.text = String(self.followArray.count)
+    }
+    
+    //フォロワーに関するデリゲートメソッド
+    func getFollowersData(followersArray: [FollowerModel], exist: Bool) {
+        
+        self.followerArray = []
+        self.followerArray = followersArray
+        
+        //フォロワー数を反映させる
+        followerLabel.text = String(followerArray.count)
+        
+        //もし、ユーザーのフォロワーに自分が入っている => すでにフォロー済みの状態だったら、ボタンの文字を変更する
+        if exist == true {
+            followButton.setTitle("フォローをやめる", for: .normal)
+        } else {
+            followButton.setTitle("フォローをする", for: .normal)
+        }
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    }
+    //投稿データを全て入手したら、tebleViewを更新するデリゲートメソッド
+    func getData(dataArray: [ContentModel]) {
+        
+        contentModelArray = dataArray
+        tableView.reloadData()
+    }
+    
+    func getProfileData(dataArray: [ProfileModel]) {
+        <#code#>
+    }
+    
+    
 }
--- RamenApp/Model/LoadModel.swift
+++ RamenApp/Model/LoadModel.swift
@@ -8,8 +8,30 @@
 import Foundation
 import Firebase
 
+//contentデータ取得時のプロトコル
 protocol GetDataProtocol {
+    
     func getData(dataArray:[ContentModel])
+    
+}
+
+protocol GetProfileDataProtocol {
+    
+    func getProfileData(dataArray:[ProfileModel])
+    
+}
+
+//相手のフォロワーに自分が存在するかどうかを判別するexist => いた場合、フォローボタンをフォロー済み状態に変更するため。
+protocol GetFollowersDataProtocol {
+    
+    func getFollowersData(followersArray:[FollowerModel], exist:Bool)
+    
+}
+
+protocol GetFollowDataProtocol {
+    
+    func getFollowData(followArray:[FollowModel])
+    
 }
 
 class LoadModel {
@@ -18,9 +40,19 @@ class LoadModel {
     
     //fireStoreから取得したデータを、配列で取得するため
     var contentModelArray:[ContentModel] = []
+    var profileModelArray:[ProfileModel] = []
     
     //プロトコルのインスタンス化
     var getDataProtocol:GetDataProtocol?
+    var getProfileDataProtocol:GetProfileDataProtocol?
+    var getFollowersDataProtocol:GetFollowersDataProtocol?
+    var getFollowDataProtocol:GetFollowDataProtocol?
+    
+    
+    //フォローフォロワーの人数関係のインスタンス
+    var followModelArray:[FollowModel]?
+    var followerModelArray:[FollowerModel]?
+    var ownFollowOrNot:Bool?
     
     
     //カテゴリー別でデータを受信するメソッド
@@ -44,13 +76,137 @@ class LoadModel {
                         //配列に代入
                         self.contentModelArray.append(contentModel)
                         
-                        //プロトコルメソッドを起動
-                        self.getDataProtocol?.getData(dataArray: self.contentModelArray)
-                        
-                    
                     }
                 }
+                //プロトコルメソッドを起動
+                self.getDataProtocol?.getData(dataArray: self.contentModelArray)
+                
             }
         }
     }
+    
+    //プロフィール画面で、ユーザー別の投稿データを取得する際に使用
+    func loadOwnContents(userID:String) {
+        
+        db.collection("Users").document(userID).collection("ownContents").order(by: "date").addSnapshotListener { [self] snapShot, error in
+            
+            self.contentModelArray = []
+            if let snapShotDoc = snapShot?.documents{
+                
+                for doc in snapShotDoc {
+                    
+                    let data = doc.data()
+                    
+                    //もし必要とするデータがfireStoreに存在し、入手することができたら
+                    if let userID = data["userID"] as? String, let userName = data["userName"] as? String, let image = data["image"] as? String,let shopName = data["shopName"] as? String,let review = data["review"] as? String,let price = data["price"] as? String,let sender = data["dender"] as? [String],let rate = data["rate"] as? Double, let date = data["date"] as? Date{
+                        
+                        //データが入ったモデルをインスタンス化
+                        let contentModel = ContentModel(userName: userName, userID: userID, price: price, shopName: shopName, review: review, imageURLString: image, rate: rate, sender: sender)
+                        
+                        //配列に代入
+                        self.contentModelArray.append(contentModel)
+                    }
+                }
+                //プロトコルメソッドを起動
+                self.getDataProtocol?.getData(dataArray: self.contentModelArray)
+            }
+        }
+    }
+    
+    func loadProfile(userID:String) {
+        
+        db.collection("Users").document(userID).addSnapshotListener { snapShot, error in
+            
+            self.profileModelArray = []
+            
+            if error != nil{
+                print(error.debugDescription)
+                return
+            }
+            
+            if let snapShotDoc = snapShot?.data() {
+                
+                if let userName = snapShotDoc["UserName"] as? String, let userID = snapShotDoc["UserID"] as? String, let image = snapShotDoc["image"] as? String, let profileText = snapShotDoc["profileText"] as? String {
+                    
+                    let profileModel = ProfileModel(userName: userName, userID: userID, profileText: profileText, imageURLString: image)
+                    
+                    self.profileModelArray.append(profileModel)
+                }
+            }
+            //プロフィール画面のフォローボタンの表記を変更するプロトコルメソッド
+            self.getProfileDataProtocol?.getProfileData(dataArray: self.profileModelArray)
+        }
+    }
+    
+    //フォロワーの情報のみを集めるメソッド(ここでフォロワーの詳細情報まで集めておくことで、フォロワー一覧画面を作成するときにデータをそのまま使えるので便利)
+    
+    func getFollowerData(userID:String) {
+        
+        db.collection("Users").document(userID).collection("follower").addSnapshotListener { snapShot, error in
+            
+            if error != nil {
+                print(error.debugDescription)
+                return
+            }
+            
+            if let snapShotDoc = snapShot?.documents{
+                
+                for doc in snapShotDoc {
+                    
+                    let data = doc.data()
+                    
+                    if let follower = data["follower"] as? String, let followOrNot = data["followOrNot"] as? Bool, let image = data["image"] as? String, let profileText = data["profileText"] as? String, let userID = data["userID"] as? String, let userName = data["userName"] as? String {
+                        
+                        //対象のデータが、自分をフォローしているかどうかを判定
+                        if userID == Auth.auth().currentUser?.uid{
+                            self.ownFollowOrNot = followOrNot
+                        }
+                        
+                        if followOrNot == true {
+                            let followerModel = FollowerModel(follower: follower, followOrNot: followOrNot, image: image, profileText: profileText, userID: userID, userName: userName)
+                            
+                            self.followerModelArray?.append(followerModel)
+                        }
+                    }
+                }
+                self.getFollowersDataProtocol?.getFollowersData(followersArray: self.followerModelArray!, exist: self.ownFollowOrNot!)
+            }
+            
+        }
+        
+    }
+    
+    //フォロワーの情報のみを集めるメソッド(ここでフォローの詳細情報まで集めておくことで、フォロ一覧画面を作成するときにデータをそのまま使えるので便利)
+    func getFollowData(userID:String) {
+        
+        db.collection("Users").document(userID).collection("follow").addSnapshotListener { snapShot, error in
+            
+            if error != nil {
+                print(error.debugDescription)
+                return
+            }
+            
+            if let snapShotDoc = snapShot?.documents{
+                
+                for doc in snapShotDoc {
+                    
+                    let data = doc.data()
+                    
+                    if let follow = data["follow"] as? String, let followOrNot = data["followOrNot"] as? Bool, let image = data["image"] as? String, let profileText = data["profileText"] as? String, let userID = data["userID"] as? String, let userName = data["userName"] as? String {
+                        
+                        
+                        if followOrNot == true {
+                            let followModel = FollowModel(follow: follow, followOrNot: followOrNot, image: image, profileText: profileText, userID: userID, userName: userName)
+                            
+                            self.followModelArray?.append(followModel)
+                        }
+                    }
+                }
+                self.getFollowDataProtocol?.getFollowData(followArray: self.followModelArray!)
+            }
+            
+        }
+        
+    }
 }
+
