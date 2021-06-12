c8260af8f0290ba317ee205ac6b93f7b19f4828d
Kai Aoyama <sauna.kaikai@gmail.com>
2021-06-12 23:13:33 +0900

    ProfileEdit作成準備

---
+++ RamenApp/Controller/ProfileEditViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileEditViewController.swift
+//  RamenApp
+//
+//  Created by ヘパリン類似物質 on 2021/06/12.
+//
+
+import UIKit
+
+class ProfileEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    //行うこと
+    //LoadModelから、currentUserのプロフィール情報を受信する
+    //変更ボタンを押したら、プロフィール情報を更新する。
+
+    
+    
+}
