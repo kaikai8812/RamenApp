f5aee4e2b6428bcdf218be566be2a0c0c077f1d8
Kai Aoyama <sauna.kaikai@gmail.com>
2021-06-13 14:07:32 +0900

    プロフィール編集画面、画面遷移、UI作成

--- RamenApp/Controller/ProfileEditViewController.swift
+++ RamenApp/Controller/ProfileEditViewController.swift
@@ -6,22 +6,38 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ProfileEditViewController: UIViewController {
     
     
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameTextView: UITextField!
+    @IBOutlet weak var userProfileTextView: UITextView!
+    
+    //loadModelのインスタンス化
+    let loadModel = LoadModel()
+    
+    let userDefaults = UserDefaultsEX()
+    //プロフィール情報を保存
+    var profileData:ProfileModel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        profileData = userDefaults.codable(key: "profile")
+        userNameTextView.text = profileData?.userName
+        userProfileTextView.text = profileData?.profileText
+        profileImageView.sd_setImage(with: URL(string: (profileData?.imageURLString)!), completed: nil)
+  
     }
     
     
     //行うこと
-    //LoadModelから、currentUserのプロフィール情報を受信する
     //変更ボタンを押したら、プロフィール情報を更新する。
 
+    @IBAction func profileUpdateButton(_ sender: Any) {
+    }
     
     
 }
